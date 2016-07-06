4fcaf26c872713c3f1afd32b1e8919fe8ee88220
Lucas <Lucas@Lucas-Vostro>
2016-07-05 21:48:21 -0300

    Implementação do JAX-RS

---
+++ syscrumweb/src/main/java/br/com/lfcsystems/syscrum/config/Application.java
@@ -0,0 +1,10 @@
+package br.com.lfcsystems.syscrum.config;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest")
+public class Application extends javax.ws.rs.core.Application {
+	
+	
+	
+}
---
+++ syscrumweb/src/main/java/br/com/lfcsystems/syscrum/resource/PesquisarSistemasResource.java
@@ -0,0 +1,33 @@
+package br.com.lfcsystems.syscrum.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import br.com.lfcsystems.syscrum.domain.Perfil;
+
+@Path("/sistemas")
+public class PesquisarSistemasResource {
+	
+	@GET
+	@Path("/test")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response test(@QueryParam("codigo") Integer codigo) {
+		System.out.println("Código: " + codigo);
+		
+		List<Perfil> perfis = new ArrayList<>();
+		Perfil perfil = new Perfil();
+		perfil.setId(1L);
+		perfil.setNome("Perfil A");
+		perfis.add(perfil);
+		
+		return Response.ok(perfis).build();
+	}
+	
+}
